e8f88fe0b89cf83ed7c10f60eb1d44281bcbd2a7
Justina Kuchta <justyna.kuchta@gmail.com>
2020-07-02 16:47:35 -0400

    asunchronous validation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormControl, Validators, FormArray, Form } from '@angular/forms'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit {
       'userData': new FormGroup({
         // when this.forbiddenNames is called it will not refer to the class as intended, that's why we need to bind it
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        // normal validators have a separate array from asynchronous validators
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       // here we add the controlls, which are key-value pairs
       'gender': new FormControl('other'),
@@ -37,6 +39,10 @@ export class AppComponent implements OnInit {
     (<FormArray>this.signupForm.get('hobbies')).push(control)
   }
 
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
   // custom validator, validation needs to return true or null!!!
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
     // if it is not found in the array
@@ -53,7 +59,20 @@ export class AppComponent implements OnInit {
   //   if (this.forbiddenUsernames.includes(control.value)) return {'name is not allowed': true}
   // }
 
-  get controls() {
-    return (this.signupForm.get('hobbies') as FormArray).controls;
+  // validator for asynchronous data (like when an email needs to be checked in the database)
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "test@test.com") {
+          resolve({'emailForbidden': true})
+        } else {
+          resolve(null)
+        }
+      },1500)
+    })
+    return promise
   }
+
+
+  
 }
